c4c237dccab9c23f900afbc2724e2533e84a58d1
malik <mubi.emailid@gmail.com>
2024-01-05 20:16:29 +0500

    add two new models comment and Like

---
+++ src/models/like.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
